56561518528e5cd09a705e9bc88dcd755932271a
mamichakes-alt <mamichakes@gmail.com>
2026-04-28 14:30:35 -0600

    cambios

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function mostrarmensaje (){
         mensaje.style.transform = "traslateY(0";
     }, 50);
 }
+function mostrarmensaje2 (){
+    let mensaje = document.getElementById("mensaje2");
+
+    mensaje.innerHTML = "hola soy oscar";
+    mensaje.style.display = "block";
+
+    setTimeout (() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "traslateY(0";
+    }, 50);
+}
